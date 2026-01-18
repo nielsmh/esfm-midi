7d4e1571a4ad707f030c1708a24395dada378094
Niels Martin Hansen <nielsm@indvikleren.dk>
2026-01-18 18:53:18 +0100

    MIDI message handling of sorts

--- src/midi.c
+++ src/midi.c
@@ -16,12 +16,26 @@ static unsigned MPU_IRQ = 9;
 
 static const unsigned MIDI_BUFSIZE = 512;
 static char __far *midi_buffer = NULL;
-static unsigned midi_buffer_read = 0;
-static unsigned midi_buffer_write = 0;
+static volatile unsigned midi_buffer_read = 0;
+static volatile unsigned midi_buffer_write = 0;
 
-int midi_in_activity;
+int volatile midi_in_activity;
 
 static void (__interrupt __far *mpu_prev_intr)();
+
+
+struct {
+    /** Status byte last seen */
+    unsigned char status_byte;
+    /** Message data currently being received */
+    unsigned char data[2];
+    /** Reading position for message being received:
+     *  0 = received status, waiting data[0]
+     *  1 = received status and data[0], waiting data[1]
+     */
+    unsigned char pos;
+} midi_inputstatus = { 0 };
+
 
 static inline unsigned char mpu_busy_read(void)
 {
@@ -86,16 +100,138 @@ void midi_close(void)
     free(midi_buffer);
 }
 
-int midi_hasinput(void)
+/** Get number of bytes available in MIDI input buffer */
+static inline int midi_numinput(void)
 {
-    return midi_buffer_read != midi_buffer_write;
+    int w = midi_buffer_write;
+    /* If the read index is past the write index in the ring buffer,
+     * the write has wrapped around, so adjust by buffer length */
+    if (midi_buffer_read > w) w += MIDI_BUFSIZE;
+    return w - midi_buffer_read;
 }
 
-unsigned char midi_read(void)
+/** Read a byte of MIDI input from offset ofs */
+static inline unsigned char midi_getinput(int ofs)
 {
-    unsigned char data = midi_buffer[midi_buffer_read];
-    ++midi_buffer_read;
-    if (midi_buffer_read == MIDI_BUFSIZE) midi_buffer_read = 0;
-    return data;
+    int r = midi_buffer_read + ofs;
+    if (ofs >= MIDI_BUFSIZE) ofs -= MIDI_BUFSIZE;
+    return midi_buffer[r];
+}
+
+/** Move the MIDI input read pointer ahead by count */
+static inline void midi_consumeinput(int count)
+{
+    midi_buffer_read += count;
+    while (midi_buffer_read >= MIDI_BUFSIZE) midi_buffer_read -= MIDI_BUFSIZE;
+}
+
+int midi_handleinput(MIDI_Input *inputdata)
+{
+    int available;
+    unsigned char data, channel;
+
+    /* Get amount of data available, and do early return check */
+    available = midi_numinput();
+    if (available == 0) return 0;
+
+    /* Get some data */
+    data = midi_getinput(0);
+    midi_consumeinput(1);
+
+    /* Check if it's a status byte */
+    if (data & 0x80) {
+        /* Check for system realtime messages (ignore those, even Reset) */
+        if (data >= 0xF8) return 0;
+        /* Otherwise store the status byte to handle in next iteration */
+        midi_inputstatus.status_byte = data;
+        midi_inputstatus.pos = 0;
+        return 0;
+    }
+
+    /* Must be a data byte then, process per status */
+    channel = midi_inputstatus.status_byte & 0x0F;
+    switch (midi_inputstatus.status_byte >> 4) {
+        case 0x8: /* Note off */
+        case 0x9: /* Note on */
+            /* Append data */
+            midi_inputstatus.data[midi_inputstatus.pos++] = data;
+            /* Check if message was completed */
+            if (midi_inputstatus.pos == 2) {
+                midi_inputstatus.pos = 0;
+                inputdata->note_on_off.channel = channel;
+                inputdata->note_on_off.note = midi_inputstatus.data[0];
+                inputdata->note_on_off.velocity = midi_inputstatus.data[1];
+                /* Velocity zero, or bit 4 not set, means it's a note off */
+                if (inputdata->note_on_off.velocity == 0 || (midi_inputstatus.status_byte & 0x10) == 0) {
+                    inputdata->note_on_off.command = MCMD_NOTEOFF;
+                } else {
+                    inputdata->note_on_off.command = MCMD_NOTEON;
+                }
+                return 1;
+            } else {
+                return 0;
+            }
+
+        case 0xA: /* Aftertouch */
+            /* Message is ignored, just count bytes */
+            midi_inputstatus.data[midi_inputstatus.pos++] = data;
+            if (midi_inputstatus.pos == 2) midi_inputstatus.pos = 0;
+            return 0;
+
+        case 0xB: /* Controller */
+            /* Append data */
+            midi_inputstatus.data[midi_inputstatus.pos++] = data;
+            /* Check if message was completed */
+            if (midi_inputstatus.pos == 2) {
+                midi_inputstatus.pos = 0;
+                inputdata->controller.command = MCMD_CONTROLLER;
+                inputdata->controller.channel = channel;
+                inputdata->controller.controller = midi_inputstatus.data[0];
+                inputdata->controller.value = midi_inputstatus.data[1];
+                return 1;
+            } else {
+                return 0;
+            }
+
+        case 0xC: /* Program */
+            inputdata->program_change.channel = channel;
+            inputdata->program_change.program = data;
+            inputdata->program_change.command = MCMD_PROGRAM_CHANGE;
+            return 1;
+
+        case 0xD: /* Pressure */
+            /* Message is ignored, just ignore bytes */
+            return 0;
+
+        case 0xE: /* Pitch */
+            /* Append data */
+            midi_inputstatus.data[midi_inputstatus.pos++] = data;
+            /* Check if message was completed */
+            if (midi_inputstatus.pos == 2) {
+                midi_inputstatus.pos = 0;
+                inputdata->pitchbend.command = MCMD_PITCHBEND;
+                inputdata->pitchbend.channel = channel;
+                inputdata->pitchbend.value = ((int)midi_inputstatus.data[0] | ((int)midi_inputstatus.data[1] << 7)) - 0x2000;
+                return 1;
+            } else {
+                return 0;
+            }
+
+        case 0xF: /* System */
+            switch (channel) {
+                case 0x0: /* System exclusive */
+                case 0x7: /* System exclusive end */
+                    /* Ignore, for now */
+                    return 0;
+
+                default:
+                    /* Pretend they don't exist */
+                    return 0;
+            }
+
+        default:
+            /* ??? */
+            return 0;
+    }
 }
 
--- src/midi.h
+++ src/midi.h
@@ -2,13 +2,64 @@
 #ifndef MIDI_H
 #define MIDI_H
 
+enum MIDI_Command {
+    MCMD_NOTEON,
+    MCMD_NOTEOFF,
+    MCMD_CONTROLLER,
+    MCMD_PARAMETER,
+    MCMD_PITCHBEND,
+    MCMD_PROGRAM_CHANGE,
+};
 
+struct MIDI_Input_NoteOnOff {
+    enum MIDI_Command command;
+    unsigned char channel;
+    unsigned char note;
+    unsigned char velocity;
+};
+
+struct MIDI_Input_Controller {
+    enum MIDI_Command command;
+    unsigned char channel;
+    unsigned char controller;
+    unsigned char value;
+};
+
+
+struct MIDI_Input_ProgramChange {
+    enum MIDI_Command command;
+    unsigned char channel;
+    unsigned char program;
+};
+
+struct MIDI_Input_Pitchbend {
+    enum MIDI_Command command;
+    unsigned char channel;
+    int value;
+};
+
+typedef union {
+    enum MIDI_Command command;
+    struct MIDI_Input_NoteOnOff note_on_off;
+    struct MIDI_Input_Controller controller;
+    struct MIDI_Input_ProgramChange program_change;
+    struct MIDI_Input_Pitchbend pitchbend;
+} MIDI_Input;
+
+
+/** Initialize MIDI input.
+ *  Returns 0 if failed, 1 if success.
+ */
 int midi_init(void);
+/** Shut down MIDI input */
 void midi_close(void);
-int midi_hasinput(void);
-unsigned char midi_read(void);
+/** Check for and return any received MIDI input.
+ *  Returns 1 if a message was received and returned in the passed-in buffer.
+ *  Returns 0 if there was no supported message.
+ */
+int midi_handleinput(MIDI_Input *inputdata);
 
-extern int midi_in_activity;
+extern volatile int midi_in_activity;
 
 #endif
 
